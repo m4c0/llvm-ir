457ffcbeebe9351ebac0932b791211becd1a2221
Eduardo Costa <m4c0@users.noreply.github.com>
2021-12-06 18:05:12 +0000

    brainf: trying some stuff to generate IR

--- brainf/brainf.cpp
+++ brainf/brainf.cpp
@@ -1,21 +1,73 @@
 #include <iostream>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <m4c0/ce_list.hpp>
 #include <m4c0/parser/combiners.hpp>
 #include <m4c0/parser/str.hpp>
 #include <string>
 
 using namespace m4c0::parser;
 
-static constexpr const auto gt = match('<');
-static constexpr const auto lt = match('>');
-static constexpr const auto plus = match('+');
-static constexpr const auto minus = match('-');
-static constexpr const auto dot = match('.');
-static constexpr const auto comma = match(',');
+class context {
+  llvm::IRBuilder<> *m_builder;
+  llvm::Value *m_data;
+
+  llvm::Value *m_zero;
+  llvm::Value *m_one;
+
+  llvm::FunctionCallee *m_getchar;
+  llvm::FunctionCallee *m_putchar;
+
+  llvm::Value *m_ptr;
+
+  [[nodiscard]] auto gep() noexcept {
+    return m_builder->CreateInBoundsGEP(m_data, {m_zero, m_ptr});
+  }
+
+  [[nodiscard]] auto load_data() noexcept {
+    return m_builder->CreateLoad(gep());
+  }
+  void store_data(llvm::Value *v) noexcept { m_builder->CreateStore(v, gep()); }
+
+  [[nodiscard]] auto dec(llvm::Value *v) noexcept {
+    return m_builder->CreateSub(v, m_one);
+  }
+  [[nodiscard]] auto inc(llvm::Value *v) noexcept {
+    return m_builder->CreateAdd(v, m_one);
+  }
+
+  [[nodiscard]] auto *ptr() noexcept { return m_ptr; }
+
+public:
+  void dec_ptr() noexcept { m_ptr = dec(ptr()); }
+  void inc_ptr() noexcept { m_ptr = inc(ptr()); }
+  void dec_data() noexcept { store_data(dec(load_data())); }
+  void inc_data() noexcept { store_data(inc(load_data())); }
+  void output() noexcept { m_builder->CreateCall(*m_putchar, {load_data()}); }
+  void input() noexcept { store_data(m_builder->CreateCall(*m_getchar)); }
+
+  void begin_block() noexcept {}
+  void end_block() noexcept {}
+};
+
+using rtype = void (context::*)() noexcept;
+
+struct rtype_buffer {
+  m4c0::ce_list_node<rtype> *allocate();
+};
+using rtype_list = m4c0::ce_list<rtype, rtype_buffer>;
+
+static constexpr const auto gt = match('<') & &context::dec_ptr;
+static constexpr const auto lt = match('>') & &context::inc_ptr;
+static constexpr const auto plus = match('+') & &context::inc_data;
+static constexpr const auto minus = match('-') & &context::dec_data;
+static constexpr const auto dot = match('.') & &context::output;
+static constexpr const auto comma = match(',') & &context::input;
 static constexpr const auto lbr = match('[');
 static constexpr const auto rbr = match(']');
 
 struct block {
-  constexpr result<char> operator()(input_t) const noexcept;
+  constexpr result<rtype> operator()(input_t) const noexcept;
 };
 
 static constexpr const auto ops =
@@ -23,8 +75,8 @@ static constexpr const auto ops =
 
 static constexpr const auto many_ops = many(skip(ops));
 
-constexpr result<char> block::operator()(input_t in) const noexcept {
-  constexpr const auto p = lbr & many_ops & rbr;
+constexpr result<rtype> block::operator()(input_t in) const noexcept {
+  constexpr const auto p = lbr & many_ops & rbr & &context::end_block;
   return p(in);
 }
 
@@ -32,6 +84,10 @@ static auto &read_user_input(std::string &line) {
   return std::getline(std::cin, line);
 }
 int main() {
+  llvm::LLVMContext ctx;
+  llvm::Module m{"brainf", ctx};
+  llvm::IRBuilder<> builder{ctx};
+
   while (std::cin) {
     for (std::string line; read_user_input(line);) {
       const auto r = many_ops({line.data(), line.size()});
