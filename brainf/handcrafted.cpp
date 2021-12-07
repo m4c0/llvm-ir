60b8773c76efdbd1e9401c98d628d0e58873d57e
Eduardo Costa <ecosta@hubspot.com>
2021-12-07 14:15:11 +0000

    brainf: refactoring handcrafted code
    The plan is to find patterns from there and bring it to the interpreter

--- brainf/handcrafted.cpp
+++ brainf/handcrafted.cpp
@@ -6,6 +6,54 @@
 
 static constexpr const auto max_elements_in_data = 30000;
 
+struct bf_globals {
+  llvm::Value *data;
+  llvm::Value *zero;
+  llvm::Value *one;
+  llvm::FunctionCallee getchar;
+  llvm::FunctionCallee putchar;
+};
+
+class bf_ops {
+  llvm::IRBuilder<> &m_builder;
+  bf_globals m_globals;
+
+public:
+  bf_ops(llvm::IRBuilder<> &b, bf_globals g) : m_builder(b), m_globals(g) {}
+
+  [[nodiscard]] auto load_data(auto ptr) {
+    auto gep =
+        m_builder.CreateInBoundsGEP(m_globals.data, {m_globals.zero, ptr});
+    return m_builder.CreateLoad(gep);
+  }
+  void store_data(auto ptr, auto v) {
+    auto gep =
+        m_builder.CreateInBoundsGEP(m_globals.data, {m_globals.zero, ptr});
+    m_builder.CreateStore(v, gep);
+  }
+
+  void plus(auto ptr) {
+    store_data(ptr, m_builder.CreateAdd(load_data(ptr), m_globals.one));
+  }
+  void minus(auto ptr) {
+    store_data(ptr, m_builder.CreateSub(load_data(ptr), m_globals.one));
+  }
+
+  [[nodiscard]] auto inc(auto ptr) {
+    return m_builder.CreateAdd(ptr, m_globals.one, ".ptr.");
+  }
+  [[nodiscard]] auto dec(auto ptr) {
+    return m_builder.CreateSub(ptr, m_globals.one, ".ptr.");
+  }
+
+  void in(auto ptr) {
+    store_data(ptr, m_builder.CreateCall(m_globals.getchar));
+  }
+  void out(auto ptr) {
+    m_builder.CreateCall(m_globals.putchar, {load_data(ptr)});
+  }
+};
+
 int main() {
   llvm::LLVMContext ctx;
   llvm::Module m{"brainf", ctx};
@@ -19,8 +67,8 @@ int main() {
 
   auto i32 = llvm::Type::getInt32Ty(ctx);
 
-  auto zero = llvm::Constant::getIntegerValue(i32, llvm::APInt(32, 0));
-  auto one = llvm::Constant::getIntegerValue(i32, llvm::APInt(32, 1));
+  auto zero = llvm::ConstantInt::get(i32, 0);
+  auto one = llvm::ConstantInt::get(i32, 1);
 
   auto getchar = m.getOrInsertFunction("getchar", i32);
   auto putchar = m.getOrInsertFunction("putchar", i32, i32);
@@ -33,6 +81,12 @@ int main() {
   auto data_tp = llvm::ArrayType::get(i32, max_elements_in_data);
   auto data = builder.CreateAlloca(data_tp, nullptr, "data");
   llvm::Value *ptr = zero;
+
+  bf_globals g{.one = one,
+               .data = data,
+               .zero = zero,
+               .getchar = getchar,
+               .putchar = putchar};
 
   auto data_init = llvm::ConstantAggregateZero::get(data_tp);
   builder.CreateStore(data_init, data);
@@ -66,14 +120,14 @@ int main() {
     auto h_ptr = builder.CreatePHI(i32, 2, ".ptr.");
 
     builder.SetInsertPoint(body);
-    auto b_ptr = blk(static_cast<llvm::Value *>(h_ptr));
+    auto b_ptr = blk(bf_ops{builder, g}, static_cast<llvm::Value *>(h_ptr));
     builder.CreateBr(header);
 
-    {                                            // header
-      auto body_exit = builder.GetInsertBlock(); // Handles nested loops
-      builder.SetInsertPoint(header);
+    { // header
       h_ptr->addIncoming(l_ptr, block);
-      h_ptr->addIncoming(b_ptr, body_exit);
+      h_ptr->addIncoming(b_ptr, builder.GetInsertBlock());
+
+      builder.SetInsertPoint(header);
 
       auto d = load_data(h_ptr);
       auto cmp = builder.CreateICmp(llvm::CmpInst::Predicate::ICMP_EQ, d, zero);
@@ -81,10 +135,7 @@ int main() {
     }
 
     builder.SetInsertPoint(exit);
-    auto x_phi = builder.CreatePHI(i32, 1, ".xptr");
-    x_phi->addIncoming(h_ptr, header);
-
-    return x_phi;
+    return h_ptr;
   };
 
   // ++++
@@ -93,13 +144,13 @@ int main() {
   }
 
   // [>++++++++<-]
-  ptr = loop(ptr, [&](auto b_ptr) {
-    b_ptr = builder.CreateAdd(b_ptr, one, ".ptr.");
+  ptr = loop(ptr, [](bf_ops ops, auto b_ptr) {
+    b_ptr = ops.inc(b_ptr);
     for (auto i = 0; i < 8; i++) {
-      plus(b_ptr);
+      ops.plus(b_ptr);
     }
-    b_ptr = builder.CreateSub(b_ptr, one, ".ptr.");
-    minus(b_ptr);
+    b_ptr = ops.dec(b_ptr);
+    ops.minus(b_ptr);
     return b_ptr;
   });
 
@@ -107,23 +158,23 @@ int main() {
   store_data(ptr, builder.CreateCall(getchar));
 
   // [[>+.-<-]>.<,]
-  ptr = loop(ptr, [&](auto b_ptr_outer) {
-    b_ptr_outer = loop(b_ptr_outer, [&](auto b_ptr) {
+  ptr = loop(ptr, [loop](bf_ops ops, auto b_ptr_outer) {
+    b_ptr_outer = loop(b_ptr_outer, [](bf_ops ops, auto b_ptr) {
       // [>+.-<-]
-      b_ptr = builder.CreateAdd(b_ptr, one, ".ptr.");
-      plus(b_ptr);
-      builder.CreateCall(putchar, {load_data(b_ptr)});
-      minus(b_ptr);
-      b_ptr = builder.CreateSub(b_ptr, one, ".ptr.");
-      minus(b_ptr);
+      b_ptr = ops.inc(b_ptr);
+      ops.plus(b_ptr);
+      ops.out(b_ptr);
+      ops.minus(b_ptr);
+      b_ptr = ops.dec(b_ptr);
+      ops.minus(b_ptr);
       return b_ptr;
     });
 
     // >.<,
-    b_ptr_outer = builder.CreateAdd(b_ptr_outer, one, ".ptr.");
-    builder.CreateCall(putchar, {load_data(b_ptr_outer)});
-    b_ptr_outer = builder.CreateSub(b_ptr_outer, one, ".ptr.");
-    store_data(ptr, builder.CreateCall(getchar));
+    b_ptr_outer = ops.inc(b_ptr_outer);
+    ops.out(b_ptr_outer);
+    b_ptr_outer = ops.dec(b_ptr_outer);
+    ops.in(b_ptr_outer);
     return b_ptr_outer;
   });
 
