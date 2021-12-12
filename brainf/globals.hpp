22ef1565b33df0b7a05ceae3b4e3916a854d4f37
Eduardo Costa <m4c0@users.noreply.github.com>
2021-12-12 11:17:55 +0000

    brainf: jit it up

--- brainf/globals.hpp
+++ brainf/globals.hpp
@@ -1,16 +1,23 @@
 #pragma once
+#include "jit.hpp"
 #include "llvm.hpp"
 
 namespace bf {
 class globals {
-  llvm::LLVMContext m_ctx;
-  llvm::Module m_mod;
+  std::unique_ptr<llvm::LLVMContext> m_ctx;
+  std::unique_ptr<llvm::Module> m_mod;
   llvm::legacy::FunctionPassManager m_fpm;
   llvm::legacy::PassManager m_mpm;
 
 public:
   explicit globals(const char *name)
-      : m_ctx{}, m_mod{name, m_ctx}, m_fpm{&m_mod}, m_mpm{} {
+      : m_ctx{std::make_unique<llvm::LLVMContext>()},
+        m_mod{std::make_unique<llvm::Module>(name, *m_ctx)}, m_fpm{m_mod.get()},
+        m_mpm{} {
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+
     llvm::PassManagerBuilder pmb;
     pmb.OptLevel = 3;
     pmb.SizeLevel = 0;
@@ -23,14 +30,20 @@ public:
     m_fpm.doInitialization();
   }
 
-  [[nodiscard]] int finish() noexcept {
-    m_mpm.run(m_mod);
-    m_mod.print(llvm::outs(), nullptr);
-    return llvm::verifyModule(m_mod, &llvm::errs()) ? 1 : 0;
+  [[nodiscard]] bool finish() noexcept {
+    m_mpm.run(*m_mod);
+    m_mod->print(llvm::outs(), nullptr);
+    return llvm::verifyModule(*m_mod, &llvm::errs());
+  }
+  void run(auto fn_name) noexcept {
+    jit j{};
+    j.add_module({std::move(m_mod), std::move(m_ctx)});
+    auto addr = llvm::cantFail(j.lookup(fn_name)).getAddress();
+    reinterpret_cast<void (*)()>(addr)();
   }
 
-  [[nodiscard]] auto &context() noexcept { return m_ctx; }
-  [[nodiscard]] auto &mod() noexcept { return m_mod; }
+  [[nodiscard]] auto &context() noexcept { return *m_ctx; }
+  [[nodiscard]] auto &mod() noexcept { return *m_mod; }
 
   void optimise(llvm::Function &f) noexcept { m_fpm.run(f); }
 };
--- brainf/llvm.hpp
+++ brainf/llvm.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include <llvm/ExecutionEngine/Orc/ExecutionUtils.h>
+#include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#include <llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Verifier.h>
+#include <llvm/Support/TargetSelect.h>
 #include <llvm/Transforms/IPO.h>
 #include <llvm/Transforms/IPO/PassManagerBuilder.h>
